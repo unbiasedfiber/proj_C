0a0fc0fb064d69486ca21899e6e8e57f219102d0
Nathan Arnett <arnett.nate@gmail.com>
2018-11-13 09:53:40 +0900

    preprocessor fun

---
+++ macro_test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/* cheesy preprocessing*/
+
+#define len(array) sizeof(array)/sizeof(int)
+
+#define sum(array)                              \
+        ({ double sum = 0;                      \
+           for (int i = 0; i < len(array); i++) \
+               { sum+=array[i]; }               \
+           sum;                                 \
+        })
+
+#define mean(array)                            \
+        ({ double s = sum(array);              \
+           double l = len(array);              \
+              s/l;                             \
+        })
+
+#define print_array(array)                      \
+        ({ for (int i = 0; i < len(array); i++) \
+               {printf("%d ", array[i]);}       \
+        })
+
+#define print_mean_for_(array)                                  \
+        ({ printf("%.2f is the mean of array : ", mean(array)); \
+           print_array(array);                                  \
+        })
+
+int main() {
+
+  int my_array[] = {4,6,10,5,3,2};
+
+  print_mean_for_(my_array);
+
+  return 0;
+}
