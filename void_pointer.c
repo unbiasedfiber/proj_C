983b4acbe05e1444cfd1c1b2f294183309124a98
Nathan Arnett <arnett.nate@gmail.com>
2018-11-17 17:08:13 +0900

    voidic pointer fun

---
+++ void_pointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+  double   a   = 3.141592;
+  int      b[] = {15,25,35,45};
+  char     c[] = "a string";
+
+  // voidic pointer
+  // numeric values must be dereferenced.
+  void *ptr;
+
+  ptr = &a;
+  printf("ptr -> %f\n", *((double *)ptr));
+
+  ptr = &b;
+  printf("ptr -> %d\n", *((int*)ptr));
+
+  ptr = &c;
+  printf("ptr -> %s\n", ptr);
+
+  return 0;
+}
