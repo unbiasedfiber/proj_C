7c26260eaf05df228520a96937381ff62760e910
Nathan Arnett <arnett.nate@gmail.com>
2018-12-02 11:58:50 +0900

    included standard dev

--- macro_test.c
+++ macro_test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 /* cheesy preprocessing*/
+
+#define print_array(array)                                      \
+        ({ for (int i = 0; i < len(array); i++)                 \
+               {printf("%d ", array[i]);}                       \
+        })
 
 #define len(array) sizeof(array)/sizeof(int)
 
@@ -17,21 +23,29 @@
               s/l;                                              \
         })
 
-#define print_array(array)                                      \
-        ({ for (int i = 0; i < len(array); i++)                 \
-               {printf("%d ", array[i]);}                       \
+#define standard_deviation(array)                               \
+        ({ double m = mean(array);                              \
+           double s = sum(array);                               \
+           double dev = 0;                                      \
+           for (int i = 0; i <len(array); i++)                  \
+            {                                                   \
+              double sq = (pow( (array[i] - m), 2) / s);        \
+              dev += sq;                                        \
+            }                                                   \
+            sqrt(dev);                                          \
         })
 
-#define print_mean_for_(array)                                  \
-        ({ printf("%.2f is the mean of array : ", mean(array)); \
+#define print_stats_for_(array)                                 \
+        ({ printf("Mean : %.2f\n", mean(array));                \
+           printf("Standard Devation : %.2f\n",                 \
+                   standard_deviation(array));                  \
            print_array(array);                                  \
-        })
+          })                                                    \
 
 int main() {
 
-  int my_array[] = {4,6,10,5,3,2};
+  int my_array[] = {2,2,2,200,2,2};
 
-  print_mean_for_(my_array);
-
+  print_stats_for_(my_array);
   return 0;
 }
