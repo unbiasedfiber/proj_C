093654c28e762db1be887e970cd1157926cd50fd
Nathan Arnett <arnett.nate@gmail.com>
2018-12-14 23:01:15 +0900

    display array and free array functions added

--- malloc_play.c
+++ malloc_play.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void malloc_array(int *arr[], int size, int value){
+  for (int i=0; i<size; i++){
+    arr[i] = (int*) malloc(sizeof(int)); //allocate memory for ints
+    *arr[i] = i*value;
+  }
+}
+
+void display_array(int *arr[], int size){
+  for(int i=0; i<size; i++){
+    printf("%d -- at address : %d\n", *arr[i], arr[i]);
+  }
+}
+
+void free_array(int *arr[], int size){
+  for(int i=0; i<size; i++){
+    free(arr[i]);
+    arr[i] = NULL;
+  }
+}
+
+
 int main () {
-  int *pi = (int*) malloc(sizeof(int));
 
-  *pi = 10;
+  int *ptr_array[5]; //init array of pointers to ints
 
-  printf("%d located at %d\n", *pi, pi);
-  free(pi);
-  printf("%d freed at %d\n", *pi, pi);
+  malloc_array(ptr_array, 5, 100);
+  display_array(ptr_array, 5);
+  free_array(ptr_array, 5);
 
+  if (ptr_array[4] == 0){
+    printf("\ndangling pointers resolved to null");
+  }
 }
